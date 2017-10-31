2ebd81d5d72b7f8eb08cf8c81a923fccdd1f8e6f
passaraink <31198485+passaraink@users.noreply.github.com>
2017-10-31 10:47:12 +0700

    Update Automative_Grid.py

--- Projectcozmo/func/Automative_Grid.py
+++ Projectcozmo/func/Automative_Grid.py
@@ -4,46 +4,47 @@ from cozmo.util import degrees, Pose, distance_mm, speed_mmps
 def cozmo_program(robot: cozmo.robot.Robot) :
     robot.world.request_nav_memory_map(0.5)
     deg = 45
-    while(True): 
+    while(True): #infinited loop starts here
         print("do the loop")
         try :
             event = robot.world.wait_for(cozmo.nav_memory_map.EvtNewNavMemoryMap, timeout=2)
             #print ("event = %s" % event)
-            nav_map = event.nav_memory_map
+            nav_map = event.nav_memory_map #use the nav_memory_map from the cozmo API you can check
 
-            _posX = robot.pose.position.x
-            _posY = robot.pose.position.y
+            _posX = robot.pose.position.x #get cozmo's x cordinate
+            _posY = robot.pose.position.y #get cozmo's y coordinate
             anglez = str(robot.pose.rotation.angle_z)
             atemp = anglez.split("(")
             atemp2 = atemp[1].split(" degree")
             _angle = float(atemp2[0])
             
-            print("Robot info : x="+str(_posX)+" y="+str(_posY)+" angle="+str(_angle))
-            state = scan_block(_posX, _posY, _angle, nav_map)
+            print("Robot info : x="+str(_posX)+" y="+str(_posY)+" angle="+str(_angle)) 
+            state = scan_block(_posX, _posY, _angle, nav_map) .#to find the state cozmo is in using the coordinates and angle/ direction cozmo is facing
             #state = scan_block_test(-40.29963302612305, -199.53224182128906, -87.09, nav_map)
-            if state[2] > 4:
-                robot.turn_in_place(degrees(45)).wait_for_completed()
-                print ("Turn")
+            if state[2] > 4: #check whether there is an obstacle in front of it or not
+                robot.turn_in_place(degrees(45)).wait_for_completed() #turn if there is an obstacle
+                print ("Turn") #printing turn so you know that cozmo is gonna turn
                 
-            else :
-                print ("Go")
-                robot.drive_straight(distance_mm(100), speed_mmps(100)).wait_for_completed()
+            else : #in this case, there is no obstacle near cozmo
+                print ("Go") #prints go so that ya know it'g gonna start walking
+                robot.drive_straight(distance_mm(100), speed_mmps(100)).wait_for_completed() #you know what this is
                 
-        except asyncio.TimeoutError :
+        except asyncio.TimeoutError : #for the timeout error 
             print("Time out waiting for map")
-        time.sleep(0.2)
-        if deg == 45:
+        time.sleep(0.2) #pause da program :D
+        if deg == 45: 
             deg = 90
         else:
             deg = 45
         #robot.go_to_pose(Pose(100, 100, 0, angle_z=degrees(deg)), relative_to_robot=False).wait_for_completed()
 
 
-def scan_block (posx, posy, angle, nav_map) :
-    posx = int(posx)
-    posy = int(posy)
-    state = [0,0,0]
-    sampling = 7
+def scan_block (posx, posy, angle, nav_map) : #ya so this is for scanning the block
+    posx = int(posx) #x coordinate
+    posy = int(posy) #y coordinate
+    state = [0,0,0] #this is to get result of the the state cozmo is in
+    sampling = 7 
+    #the following lines are to check in which angle or direction to see if there is an obstacle in front of it
     if (angle >= 0 and angle <23) or (angle < 0 and angle > -23) :
         for i in range(posx, posx + 150, sampling) :
            for j in range(posy-25, posy+25, sampling) :
@@ -102,16 +103,16 @@ def scan_block (posx, posy, angle, nav_map) :
                content = str(nav_map.get_content(i,j))
                state[convert_content(content)] += 1
     print(state)
-    return state
+    return state #state is a list
 
 
     
-def convert_content (content) :
+def convert_content (content) : 
     if(content == "NodeContentTypes.ClearOfObstacle" or content == "NodeContentTypes.ClearOfCliff") :
-        return 1
+        return 1 #if there is no obstacle or if there is no cliff, return 1 (color is green)
     elif(content == "NodeContentTypes.Unknown") :
-        return 0
+        return 0 #for when you don't know the color of the grid (no color)
     else :
-        return 2
+        return 2 #there is an obstacle (color is yellow/ orange)
     
 cozmo.run_program(cozmo_program, use_3d_viewer=True, use_viewer=True)
