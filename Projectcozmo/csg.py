a67dbf5d4a5d3cf74685e3be3280e7085f34584f
paweemew <32890875+paweemew@users.noreply.github.com>
2017-10-31 10:37:56 +0700

    Add some description
    delete some unnecessary lines

--- Projectcozmo/csg.py
+++ Projectcozmo/csg.py
@@ -1,23 +1,25 @@
+#import some file
 import cozmo
 from flask import Flask, render_template, request, url_for
+
+#create new flask named "app" and new list named "message"
 app = Flask(__name__)
 message1=[]
+
 #key words for location and picture
 sent_pic = {'pic','picture','photo','photoes'}
 sent_lo  = {'location','locate',}
 
-#main webpage
+#main webpage (run when it found '/' in the url)
 @app.route('/', methods=["GET", "POST"])
 def Cozmo(name='name'):
+  #when in found 'post' it will add that message in to the list(message1)
   if request.method == 'POST':
      message1.append(request.form['Msg'])
-  print(message1)   
+  #run cozmo.html file    
   return render_template('cozmo.html', name=name, message1 = message1,sent_pic = sent_pic,sent_lo = sent_lo)
 
-@app.route('/try')
-def tried():
-  return render_template('try.html')
-
+#main(run the program)
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
