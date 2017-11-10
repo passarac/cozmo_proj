dd0800e839b903e35afe161bd2a809f0fea01a2a
passaraink <31198485+passaraink@users.noreply.github.com>
2017-11-10 15:25:36 +0700

    Update cozmo picture.py

--- Projectcozmo/func/cozmo picture.py
+++ Projectcozmo/func/cozmo picture.py
@@ -2,12 +2,12 @@ import cozmo, time, asyncio
 from cozmo.util import degrees, Pose, distance_mm, speed_mmps
 from PIL import Image
 
-def cozmo_take_pic(robot: cozmo.robot.Robot) :
-    cozmo.camera.Camera.image_stream_enabled = True
-    latest_image = robot.world.latest_image
-    img = latest_image.annotate_image()
-    print (latest_image)
-    img.show()
+def cozmo_take_pic(robot: cozmo.robot.Robot) :  
+    cozmo.camera.Camera.image_stream_enabled = True #set to true so that images can be received
+    latest_image = robot.world.latest_image #get the latest image from Cozmo's camera
+    img = latest_image.annotate_image() #resize it, this will return an image
+    print (latest_image) #not necessary
+    img.show() #show the image
 
 
 cozmo.run_program(cozmo_take_pic,use_viewer=True)
