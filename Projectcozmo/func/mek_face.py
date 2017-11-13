87bed60c3a1ad1ebade21e96498f434b8996a29c
paweemew <32890875+paweemew@users.noreply.github.com>
2017-11-13 11:37:39 +0700

    mek update

--- Projectcozmo/func/mek_face.py
+++ Projectcozmo/func/mek_face.py
@@ -9,14 +9,17 @@ def cozmo_take_pic(robot: cozmo.robot.Robot) :
     cozmo.camera.Camera.image_stream_enabled = True #set to true so that images can be received
     facetaken = []
 
-    if visible_face not in facetaken:
-        facetaken.append(visible_face)
-        latest_image = robot.world.latest_image #get the latest image from Cozmo's camera
-        img = latest_image.annotate_image() #resize it, this will return an image
-        print (latest_image) #not necessary
-        img.show() #show the image
-    else:
-        print ("No face seen")
+    
+    for visible_face in robot.world.visibles_faces:
+        if visible_face not in facetaken:
+            facetaken.append(visible_face)
+            latest_image = robot.world.latest_image #get the latest image from Cozmo's camera
+            img = latest_image.annotate_image() #resize it, this will return an image
+            print (latest_image) #not necessary
+            img.show() #show the image
+    
+        else:
+            print ("No face seen")
         
 
 
