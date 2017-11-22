b7704941627d7ae215d44f8ea4c578a64934daff
paweemew <32890875+paweemew@users.noreply.github.com>
2017-11-22 11:55:22 +0700

    eiei
    ss

--- Projectcozmo/csg.py
+++ Projectcozmo/csg.py
@@ -1,35 +1,52 @@
-#import cozmo
-from flask import Flask, render_template, request, url_for
-import requests
-#from multiprocessing import Pool
-#from func import remember_face
-#cozmo.run_program(remember_face.cozmo_program, use_3d_viewer=False, use_viewer=False)
+import cozmo
+import os
+import subprocess
+import time
+from flask import Flask, render_template, request, url_for,redirect
+from flask import send_from_directory
+from werkzeug import secure_filename
+from func import remember_face
+UPLOAD_FOLDER = './people pic'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+listpic = os.listdir(r'''C:\Users\PAWEE\Desktop\multi upload case\people pic''')
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 message1=[]
-# ;key words for location and picture
+#key words for location and picture
 sent_pic = {'pic','picture','photo','photoes'}
 sent_lo  = {'location','locate',}
-a='a'
+
+def open_program(path_name):
+    return subprocess.Popen(path_name)
+def close_program(p):
+    p.terminate() 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)  
+                               
 #main webpage
 @app.route('/', methods=["GET", "POST"])
 def Cozmo(name='name'):
   if request.method == 'POST':
-     message1.append(request.form['Msg'])  
+     message1.append(request.form['Msg'])
+
   return render_template('cozmo.html', name=name, message1 = message1,sent_pic = sent_pic,sent_lo = sent_lo)
 
 @app.route('/list', methods=["GET", "POST"])
 def List(name='name'):
-  return render_template('list.html', name=name, fa = remember_face.faces_mem)
+  listpic = os.listdir(r'''C:\Users\PAWEE\Desktop\Github\Cozmo-Test\people pic''')
+  return render_template('list.html', name=name, fa = remember_face.faces_mem,listpic = listpic)
 
-@app.route('/try', methods=["GET", "POST"])
-def tried(a=a):
-  if request.method == 'POST':
-    a+=b
-    r = requests.post('http://127.0.0.1:8000/try')
-  return r.text
+@app.route('/run', methods=["GET", "POST"])
+def Run(name='name'):
+  p=open_program("") 
+  time.sleep(3)
+  close_program(p)
+  return None
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8000)
-    print("test")
-    
